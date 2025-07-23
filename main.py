e370999917997d304896697308c196c34d4bc7ee
FlowTraker <flowtrakersealab@gmail.com>
2025-07-23 16:30:08 -0500

    13c

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ async def transcribir_endpoint(file: UploadFile = File(...)):
 # 🔥 Este bloque es CLAVE para que Render use el puerto correcto
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
